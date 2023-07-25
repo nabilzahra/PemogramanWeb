a98e162ae7fd9d325863572a2472a6a34682a737
nabilzahra <140367751+nabilzahra@users.noreply.github.com>
2023-07-25 22:10:42 +0700

    aritmatika.js

---
+++ aritmatika.js
@@ -0,0 +1,13 @@
+     let bil1=8;
+     let bil2=7;
+     //Operasi Arimatika
+     let pertambahan=bil1+bil2;
+     let pengurangan=bil1-bil2;
+     let perkalian=bil1*bil2;
+     let pembagian=bil1/bil2;
+     let modulus=bil1 % bil2;
+     document.write ("Hasil Pertambahan"+pertambahan+"<br>");
+     document.write ("Hasil Pengurangan"+pengurangan +"<br>");
+     document.write ("Hasil Perkalian"+perkalian+"<br>");
+     document.write ("Hasil Pembagian"+pembagian+"<br>");
+     document.write ("Hasil Modulus"+modulus+"<br>");# PemogramanWeb
